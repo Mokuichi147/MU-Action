b5e81de29076a6ee7711f663d7b4760983b7b24b
Mokuichi147 <38586357+Mokuichi147@users.noreply.github.com>
2020-11-01 02:37:42 +0900

    [add] ボール監視用

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public TableTennisAgent Player_A;
+    public TableTennisAgent Player_B;
+    
+    private int bound_count;
+    private string pre_action;
+
+    void Start()
+    {
+        Reset();
+    }
+
+    void Reset()
+    {
+        bound_count = 0;
+        pre_action = "";
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+}
